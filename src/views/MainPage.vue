4689c9e5e05411697683502c9daff8f84b901ea1
Madhurya Mahajan <madhuryamahajan@gmail.com>
2020-05-08 16:40:40 -0400

    adding tabs and eventbus

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,6 +2,23 @@
 <div class="mainPage">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+  <!-- Top Navbar -->
+  <!-- <nav class="navbar" role="navigation" area-label="dropdown navigation">
+    <div class="navbar-item navbarcontent">
+      <h1>Personal Finances</h1>
+    </div>
+    <div id="navMenu" class="navbar-menu">
+      <a class = "navbar-link navbarcontent">
+        About
+      </a>
+      <div class="navbar-dropdown is-right">
+        <a class = "navbar-item">
+          Version
+        </a>
+      </div>
+    </div>
+  </nav> -->
+
   <!-- Side Navbar -->
   <aside class="menu"><br /><br />
     <strong class="menu-label">Pages</strong><br /><br />
@@ -20,33 +37,49 @@
 
   <!-- Main Body -->
   <h1>Personal Finances</h1>
-  <body>
-    <pie-chart width="1000" height="300"
-      id="expenditures-chart"
-      title="Expenditures"
-      type="pie"
-      :labels='["Gas", "Food", "Loans", "Groceries", "Rent"]'
-      :data='[4, 9, 3, 5, 6]'
-      :background-color='["#394E79", "#5E83BA", "#C2D2E9", "#9A8BA5", "#E3C5D5"]'
-      :border-color='white'
-      ></pie-chart>
+  <div class="tabs is-boxed">
+    <ul>
+      <li v-bind:class="{'is-active': (visibleTab == 'Main')}" v-on:click="setVisibleTab('Main')">
+        <a>Main View</a>
+      </li>
+      <li v-bind:class="{'is-active': (visibleTab == 'Monthly')}" v-on:click="setVisibleTab('Monthly')">
+        <a>Monthly View</a>
+      </li>
+      <li v-bind:class="{'is-active': (visibleTab == 'Travel')}" v-on:click="setVisibleTab('Travel')">
+        <a>Travel View</a>
+      </li>
+    </ul>
+  </div>
 
-  </body>
+  <travel-page v-show="visibleTab === 'Travel'"></travel-page>
+
 </div>
 </template>
 
 <script>
-import pieChart from '@/components/PieChart.vue'
+import EventBus from '../models/event-bus'
+import travelPage from '@/views/TravelView.vue'
+
 export default {
   name: "mainPage",
   components: {
-    pieChart,
+    travelPage,
+    // pieChart,
   },
   props: {
   },
   data() {
     return {
+      visibleTab: 'Main',
     };
+  },
+  computed:{},
+  mounted(){},
+  methods:{
+    setVisibleTab(tab){
+      this.visibleTab = tab;
+      EventBus.$emit('tabChanged', this.visibleTab)
+    },
   }
 };
 </script>
@@ -60,7 +93,6 @@ aside {
   height: 100%; /* Full-height: remove this if you want "auto" height */
   width: 200px; /* Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-
   top: 0;
   left: 0;
   background-color: rgba(222, 93, 132, 0.233);
@@ -72,12 +104,21 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 pie-chart {
   height: 200px;
   width: 200px;
+}
+.navbar{
+  height: 40px;
+  min-height: 40px;
+}
+@media all and (max-width: 1087px) {
+  .navbar-menu .navbar-item .navbarcontent {
+    background-color: white;
+  }
+}
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
